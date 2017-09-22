c49dc2bf3a08d409f573883aa1ae5bd3298fe8dc
Mike Harder <mharder@microsoft.com>
2017-09-22 15:17:47 -0700

    Improve binary-build scripts

--- aspnet/buildtools/binary-build/build.ps1
+++ aspnet/buildtools/binary-build/build.ps1
@@ -1,1 +1,9 @@
-docker build -t aspnet-build-tools:binary-build -f Dockerfile ../.. | Tee-Object docker-build.log
+$sourceDir = '../../../../buildtools'
+
+Copy-Item .dockerignore $sourceDir
+Copy-Item Dockerfile $sourceDir
+
+docker build -t aspnet-build-tools:binary-build -f $sourceDir/Dockerfile $sourceDir | Tee-Object docker-build.log
+
+Remove-Item $sourceDir/.dockerignore
+Remove-Item $sourceDir/Dockerfile
